8f78499e630dd187c1e7918deece90b85af5eb1e
Alexander-3112 <75229064+Alexander-3112@users.noreply.github.com>
2020-12-11 09:55:57 +0700

    Add files via upload

---
+++ Praktikum week 13.2/Praktikum week 13.2/Form1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Praktikum_week_13._2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (label2.Text == "[ EMPTY ]")
+            {
+                label2.Text = textBox1.Text;
+            }
+            if (label2.Text != "[ EMPTY ]" && textBox1.Text == "DELETE")
+            {
+                label2.Text = "[ EMPTY ]";
+            }
+            if (label2.Text != "[ EMPTY ]" && textBox1.Text == "RED")
+            {
+                label2.ForeColor = Color.Red;
+            }
+            if (label2.Text != "[ EMPTY ]" && textBox1.Text == "GREEN")
+            {
+                label2.ForeColor = Color.Green;
+            }
+            if (label2.Text != "[ EMPTY ]" && textBox1.Text == "BLUE")
+            {
+                label2.ForeColor = Color.Blue;
+            }
+            if (label2.Text != "[ EMPTY ]" && textBox1.Text == "HIDE")
+            {
+                label2.Hide();
+            }
+            if (label2.Text != "[ EMPTY ]" && textBox1.Text == "SHOWN")
+            {
+                label2.Visible = true;
+            }
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
